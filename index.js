810172ae1b9e00cdf94d21960a264296009e6a74
Parimal-3008 <parimalyeole30@gmail.com>
2023-05-07 14:02:55 +0530

    socket.io

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const schema = new mongoose.Schema({
   account_balance: Number,
 });
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
